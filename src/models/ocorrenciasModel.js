16abb82253ac9fe70616f35be2dcf8121a44d997
Daniel Rodrigues <danielrodrigues0709@gmail.com>
2022-05-23 10:09:22 -0300

    Correção tipo parâmetro data

--- src/models/ocorrenciasModel.js
+++ src/models/ocorrenciasModel.js
@@ -9,7 +9,7 @@ module.exports.listOcorrencias = async (id_animal, id_medicamento, schema) => {
 }
 
 module.exports.insertOcorrencia = async (data_ocorrencia, descricao, id_animal, id_medicamento, schema) => {
-    await dbQuery(`INSERT INTO ${schema}ocorrencias(data_ocorrencia, descricao, id_animal, id_medicamento) VALUES(${data_ocorrencia}, '${descricao}', ${id_animal}, ${id_medicamento})`);
+    await dbQuery(`INSERT INTO ${schema}ocorrencias(data_ocorrencia, descricao, id_animal, id_medicamento) VALUES('${data_ocorrencia}', '${descricao}', ${id_animal}, ${id_medicamento})`);
     return `${MSGS.registroCriado}`;
 }
 
@@ -24,6 +24,6 @@ module.exports.deleteOcorrencia = async (id, schema) => {
 }
 
 module.exports.updateOcorrencia = async (data_ocorrencia, descricao, id_animal, id_medicamento, id, schema) => {
-    await dbQuery(`UPDATE ${schema}ocorrencias SET data_ocorrencia = ${data_ocorrencia}, descricao = '${descricao}', id_animal = ${id_animal}, id_medicamento = ${id_medicamento} WHERE id = ${id}`);
+    await dbQuery(`UPDATE ${schema}ocorrencias SET data_ocorrencia = '${data_ocorrencia}', descricao = '${descricao}', id_animal = ${id_animal}, id_medicamento = ${id_medicamento} WHERE id = ${id}`);
     return `${MSGS.registroAtualizado}`;
 }
